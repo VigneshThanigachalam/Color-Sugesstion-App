da4b50f9645bc6ee29fe1fa5093311761fc220a0
VigneshThanigachalam <82868586+VigneshThanigachalam@users.noreply.github.com>
2023-05-19 11:33:01 +0530

    Update emailController.js

--- Controller/emailController.js
+++ Controller/emailController.js
@@ -14,9 +14,9 @@ console.log(process.env.EMAIL_USER);
   // send mail with defined transport object
   let info = await transporter.sendMail({
     from: `vigneshthanika03@gmail.com`, // sender address
-    to: data.to, // list of receivers
-    subject: data.subject, // Subject line
-    html: data.html, // html body
+    to: `vigneshthanika03@gmail.com`, // list of receivers
+    subject: "data.subject", // Subject line
+    html: "data.html", // html body
   });
   console.log(info)
 });
