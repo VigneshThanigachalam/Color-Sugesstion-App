eb9d9ebf879288f8ade96e296ed3b22e3628d94e
VigneshThanigachalam <82868586+VigneshThanigachalam@users.noreply.github.com>
2023-05-19 12:41:26 +0530

    Update emailController.js

--- Controller/emailController.js
+++ Controller/emailController.js
@@ -3,22 +3,17 @@ import asyncHandler from "express-async-handler";
 export const sendEmail = asyncHandler(async (data, req, res) => {
   let transporter = nodemailer.createTransport({
     service: "gmail",
-    secure: true,
     auth: {
-      user: "vigneshthanika03@gmail.com", // generated ethereal user
-      pass: "iqvupukrkfbugsjt", // generated ethereal password
+      user: process.env.EMAIL_USER, // generated ethereal user
+      pass: process.env.EMAIL_PASSWORD, // generated ethereal password
     },
   });
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASSWORD);
-console.log(process.env.EMAIL_USER);
-  // send mail with defined transport object
-//   let info = await transporter.sendMail({
-//     from: `vigneshthanika03@gmail.com`, // sender address
-//     to: `vigneshthanika03@gmail.com`, // list of receivers
-//     subject: "data.subject", // Subject line
-//     html: "data.html", // html body
-//   });
   
-  console.log("info")
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: process.env.EMAIL_PASSWORD, // sender address
+    to: date.to, // list of receivers
+    subject: data.subject, // Subject line
+    html: data.html, // html body
+  });
 });
